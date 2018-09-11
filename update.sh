23c7c574c078260f2cd253c44a808cd33f8a8270
Elliot Hou, Ao-ping <bill200491@gmail.com>
2018-09-11 09:35:52 +0800

    Commit for Tue Sep 11 09:35:52 CST 2018

--- update.sh
+++ update.sh
@@ -11,6 +11,16 @@ cd ..
 cp -r ../../elliot.mutual/fundList.json .
 cd ..
 
+dirName=180901
+cd mutual
+rm -rf $dirName
+mkdir $dirName && cd $dirName
+cp -r ../../../elliot.mutual/$dirName/jpmorgan* .
+cp -r ../../../elliot.mutual/$dirName/simList.json .
+cd ..
+cp -r ../../elliot.mutual/fundList.json .
+cd ..
+
 # tar cvf reports.tar currency  mutual 
 # bzip2 --best reports.tar
 
